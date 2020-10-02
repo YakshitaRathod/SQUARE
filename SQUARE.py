eb0998036b02c08adee3fb058bb7e225982f4923
YakshitaRathod <72180197+YakshitaRathod@users.noreply.github.com>
2020-10-03 04:20:50 +0530

    Update SQUARE.py

--- SQUARE.py
+++ SQUARE.py
@@ -1,4 +1,3 @@
 # FIND THE SQUARE OF A NUMBER TAKEN FROM THE USER
-while(True):
-    var1 = int(input ("ENTER A NUMBER TO FIND ITS SQUARE\n"))
-    print("THE SQUARE OF", var1,"IS =", int(var1 * var1))
+var1 = int(input ("ENTER A NUMBER TO FIND ITS SQUARE\n"))
+print("THE SQUARE OF", var1,"IS =", int(var1 * var1))
